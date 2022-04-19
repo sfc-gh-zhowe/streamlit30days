46151d1f306bf465cd5080dec8102409702bc176
Zack Howe <zackhowe@gmail.com>
2022-04-19 12:21:36 -0400

    Day 11

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,30 @@ def day_eleven():
 
      st.write('You selected:', options)
 
+def day_twelve():
+     st.header('st.checkbox')
+
+     st.write ('What would you like to order?')
+
+     icecream = st.checkbox('Ice cream')
+     coffee = st.checkbox('Coffee')
+     cola = st.checkbox('Cola')
+
+     if icecream:
+          st.write("Great! Here's some more 🍦")
+
+     if coffee: 
+          st.write("Okay, here's some coffee ☕")
+
+     if cola:
+          st.write("Here you go 🥤")
+
+
+
+
+
+
+
 
 
 if 'slider_value' not in st.session_state:
